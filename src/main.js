dbea8740090246643295429e860d8913add53370
Caleb <73370112+CHemmestad@users.noreply.github.com>
2025-01-17 16:59:24 -0600

    still works

--- src/main.js
+++ src/main.js
@@ -6,13 +6,6 @@ import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader.js';
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
 import Thanks from '/public/images/thanks.glb';
 
-const myModal = document.getElementById('myModal')
-const myInput = document.getElementById('myInput')
-
-myModal.addEventListener('shown.bs.modal', () => {
-  myInput.focus()
-})
-
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({
